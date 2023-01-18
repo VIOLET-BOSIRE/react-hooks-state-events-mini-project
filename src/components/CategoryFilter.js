d40b01772b64bc00535cf7a99e44ab21ec899d22
“VIOLET-BOSIRE” <violet.bosire@student.moringaschool.com>
2023-01-18 12:32:08 +0300

    completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,31 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, allTasks, setTasksToShow}) {
+
+  function handleCategoryClick(event){
+    const category = event.target.textContent
+    event.target.parentElement.querySelectorAll('button').forEach(btn=> btn.classList.remove("selected"))
+    event.target.classList.add("selected")
+
+    if(category === "All"){
+      setTasksToShow(allTasks)
+    }else{
+      const newTasksToShow = allTasks.filter(task => {
+        return task.category === category
+      })
+  
+      setTasksToShow(newTasksToShow)
+    }
+  }
+
+  const buttons = categories.map(category => {
+    return <button onClick={handleCategoryClick}>{category}</button>
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {buttons}
     </div>
   );
 }
